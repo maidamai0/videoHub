6e1e24545beb17a3c224021036ee99893bb1ec6b
tonghao.yuan <yuantonghao@gmail.com>
2020-03-15 23:04:48 +0800

    feat:add settings

--- src/model/configuration.cpp
+++ src/model/configuration.cpp
@@ -1,22 +1,42 @@
 #include "configuration.h"
 
+#include <QDebug>
 #include <fstream>
 #include <iostream>
 #include <string>
 
-// #include "qml_type_register.h"
-
-// namespace {
-// QMLTypeRegister<ConfigModel> register_;
-// }
+#include "toml11/toml.hpp"
+#include "toml11/toml/parser.hpp"
 
 namespace {
-const std::string config_file_name{"config.toml"};
+const std::string config_file_name{"settings.toml"};
 }  // namespace
 
 ConfigModel::ConfigModel() {
+  auto settings = toml::parse(config_file_name);
+
+  auto general = settings["general"];
+  SetLogLevel(settings["general"]["log_level"].as_integer());
+
+  auto network = settings["network"];
+  SetHttpProxy(QString::fromStdString(network["http_proxy"].as_string()));
+  SetHttpsProxy(QString::fromStdString(network["https_proxy"].as_string()));
+
+  Print();
+
   connect(this, &ConfigModel::httpProxyChanged, [this]() {
     std::cout << __FILE__ << ":" << __LINE__ << ":" << httpProxy_.toStdString()
               << std::endl;
   });
+}
+
+void ConfigModel::Print() {
+  qDebug() << "settings:";
+
+  qDebug() << "general:";
+  qDebug() << "log level: " << logLevel_;
+
+  qDebug() << "network:";
+  qDebug() << "http proxy: " << httpProxy_;
+  qDebug() << "https proxy: " << httpsProxy_;
 }
--- src/model/configuration.h
+++ src/model/configuration.h
@@ -18,9 +18,12 @@
 class VH_EXPORT ConfigModel : public QObject {
   Q_OBJECT
   QPROP(QString, httpProxy, HttpProxy);
+  QPROP(QString, httpsProxy, HttpsProxy);
+  QPROP(int, logLevel, LogLevel);
 
  public:
   ConfigModel();
+  void Print();
 
  private:
 };
