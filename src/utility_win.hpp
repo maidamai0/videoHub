ac232c2ee2f28c72d36a7a4f721384f430e05c23
tonghao,yuan <yuantonghao@gmail.com>
2020-09-14 21:27:12 +0800

    fix: :bug: fix compile error on vs2019

--- src/utility_win.hpp
+++ src/utility_win.hpp
@@ -1,17 +1,19 @@
 /**
  * @file utility_win.hpp
  * @author yuantonghao@gmail.com
- * @brief   utility functions only workd on windows platform. 
+ * @brief   utility functions only workd on windows platform.
  * @version 0.1
  * @date 2020-09-14
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
 
+#include <Windows.h>
+#include <objbase.h>
+#include <shellapi.h>
 #include <shlobj.h>
 #include <shlwapi.h>
-#include <objbase.h>
 
 #include <string>
 
